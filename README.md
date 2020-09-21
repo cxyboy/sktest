3b2bf7635ebf40c6fa8abba67c633f692efd00e3
cxyboy <791297575@qq.com>
2020-09-21 15:18:19 +0800

    del venv

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # sktest
-关键字ui测试框架
+基于selenium的关键字ui快速测试框架,旨在让测试不用再花费大量的时间去编写测试代码或维护代码，只需要设计好你的测试用例，自动化的执行交给我们。
+1. 使用excel编写并维护用例
+2. 以业实际务流程驱动测试
+3. 定位每一个业务步骤的执行情况
+4. 自动生成测试报告，并将用例执行情况及报告发送到指定邮箱
+***
+# 运行环境
+- 操作系统：Windows
+- python版本：3.5+
+- 浏览器：兼容chrome（默认）、Firefox、ie
+- 注：请确保你的driver在环境变量PATH,或者你应该指定executable_path
+***
+# 安装
+> pip install sktest
+***
+# 快速使用
+1. 新建一个测试项目
+2. 安装sktest
+3. 在项目下新建两个目录---testcase、report
+4. 将测试用例的excel文件放入testcase目录下
+5. 新建一个py文件，编写代码:'from sktest import run'
+6. 运行这个py文件
+***
+## 测试用例示例
+![case](http://m.qpic.cn/psc?/V54ePMDp3lDEZw32DPWK2XO6Tg3SZLRl/bqQfVz5yrrGYSXMvKr.cqbQQiYgAxkNv38AXv9gnccg2IsXqCs9QT2tlRC2PLHPBkWrD5HqJGWlkUGG.qPTpzms2NUzK*sNem3FaDRKvREg!/b&bo=wgREAwAAAAADB6M!&rf=viewer_4)
+![elements](http://m.qpic.cn/psc?/V54ePMDp3lDEZw32DPWK2XO6Tg3SZLRl/bqQfVz5yrrGYSXMvKr.cqSFcGtB5ax2hODaPrUSJ3QekZao.31sONPhDVpXB1SYZEqNBd1nKQZzTW1Q*eUDVcyXz5OmzdLio0YW4gDp94OM!/b&bo=sANEAwAAAAADB9Y!&rf=viewer_4)
+- 重点：
+    - 编写用例的sheet 名字必须是你所指定sheet_name，如果没有配置默认是 'case'
+    - 存放元素的sheet 名字必须是 'elements'
+    - 用例表单页面和元素列的名字必须和元素表page和element保持一致
+***
+# 自定义配置信息
+    或者你可以尝试自己配置一些信息，在你创建的py文件修改这些配置信息
+1. 自行配置测试用例文件的路径(cfn.excel_file_path)，无需再创建testcase目录
+2. 配置浏览器及driver(cfn.browser_name、cfn.executable_path)
+3. 配置邮件信息(cfn.email_account、email_password、email_receiver、case_object_name)
--- run.py
+++ run.py
@@ -6,31 +6,42 @@ import os
 from datetime import datetime
 from sktest.autotest import AutoTest
 from common.emailtool import send_email
+from common.log import logger
 
-case_object_name = 'Your Object name'
 
-file_path = 'testcase/testcase.xlsx'
-sheet_name = 'case'
+class Config:
+    def __init__(self, excel_file_path='testcase/testcase.xlsx', case_sheet_name='case', browser_name="Chrome",
+                 executable_path='', case_object_name='UI Automation testing', email_account='', email_password='',
+                 email_receiver=None):
+        self.excel_file_path = excel_file_path
+        self.case_sheet_name = case_sheet_name
+        self.browser_name = browser_name
+        self.executable_path = executable_path
+        self.case_object_name = case_object_name
+        self.email_accuont = email_account
+        self.email_password = email_password
+        self.email_receiver = email_receiver
 
-chrome_executable_path = ''
-ie_executable_path = ''
 
-browser_config = {"browserName": "Chrome", "executable_path": chrome_executable_path}
+cfg = Config()
 
-auto = AutoTest(file_path, sheet_name, browser_config)
+browser_config = {"browserName": cfg.browser_name, "executable_path": cfg.executable_path}
+email_config = {'user': cfg.email_accuont, "password": cfg.email_password}
+
+
+auto = AutoTest(cfg.excel_file_path, cfg.case_sheet_name, browser_config)
 run_time = datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M:%S")
 auto.run()
 
-email_config = {'user': "Your email", "password": "Email authorization code"}
-email_receiver = ['18573208753@163.com']
+receiver = cfg.email_receiver
 email_content = f"""
-        测试项目：{case_object_name}
+        测试项目：{cfg.case_object_name}
         测试环境：{os.name}
         执行时间：{run_time}
         测试执行人：{os.getlogin()}
         自动化测试完成，详情请查阅附件。。。
     """
-send_email(email_config, email_receiver, email_content)
+send_email(email_config, receiver, email_content)
 
 # TODO 后续扩展：
 #                1.错误截图 --  done 出错截取整个屏幕
